d444499e948e34c7f8ffccc4ad3abd3e51dc02e0
Olga Kulikova <chikylikova2016@yandex.ru>
2020-11-14 22:01:41 +0300

    доделать второй попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,95 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close');
-let title = document.querySelector('.profile__info-name');
-let subTitle = document.querySelector('.profile__info-work');
-let form = document.querySelector('.popup__form');
-let nameField = document.querySelector('.popup__field_type_name');
-let titleField = document.querySelector('.popup__field_type_title');
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const popup = document.querySelector('.popup');
+const popupAddCards = document.querySelector('.popup_type_edit');
+
+const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
+const closeButton = document.querySelector('.popup__close');
+const closeAddButton = document.querySelector('.popup__close_add-button');
 
-function showPopup() {
-    popup.classList.add('popup_opened');
+const title = document.querySelector('.profile__info-name');
+const subTitle = document.querySelector('.profile__info-work');
+const elementTitle = document.querySelector('.element__title');
+
+const form = document.querySelector('.popup__form');
+
+const nameField = document.querySelector('.popup__field_type_name');
+const titleField = document.querySelector('.popup__field_type_title');
+const placeField = document.querySelector('.popup__field_type_place');
+
+const popupImage = document.querySelector('.popup_type_image');
+
+
+
+function closePopup(popup) { 
+    popup.classList.remove('popup_opened'); 
+} 
+
+
+function submitForm(event) { 
+    event.preventDefault(); 
+    title.textContent = nameField.value; 
+    subTitle.textContent = titleField.value; 
+    closePopup(popup); 
+} 
+
+form.addEventListener('submit', submitForm); 
+ 
+function showPopup(popup) { 
+    popup.classList.add('popup_opened'); 
+    nameField.value = title.textContent; 
+    titleField.value = subTitle.textContent; 
+} 
+
+
+editButton.addEventListener('click', () => showPopup(popup)); 
+closeButton.addEventListener('click', () => closePopup(popup)); 
+addButton.addEventListener('click', () => showPopup(popupAddCards));
+closeAddButton.addEventListener('click', () => closePopup(popupAddCards));
+
+
+/*const addCards = document.querySelector('.element');
+const cardTemplate = document.querySelector('#template-card').content.querySelector('.element__card');
+
+const createCard = (card) => {
+    const cardElement = cardTemplate.cloneNode(true);
+    const photo = cardElement.querySelector('.element__image');
+    const title = cardElement.querySelector('.element__title');
+
+    title.textContent = card.name;
+    photo.style.backgroundImage = `url{$(card.link)}`;
+
+    return createCard;
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
+const renderCreateCard = (card, element) => {
+    element.prepend(createCard(card))
+};
 
-editButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', closePopup);
-addButton.addEventListener('click', showPopup);
-
-function submitForm(event) {
-    event.preventDefault();
-    title.textContent = nameField.value;
-    subTitle.textContent = titleField.value;
-}
-
-form.addEventListener('submit', submitForm);
+initialCards.forEach( card => renderCreateCard(card, addCards));*/
